f42c1a03f119f412900a35b48f2134c51cf4b924
nataliastewart <nataliastewart@users.noreply.github.com>
2020-04-27 17:07:20 -0400

    Added styling to the home page and Nav anchors

--- lambdalist/src/App.js
+++ lambdalist/src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import "./App.css";
 import NavBar from "./Components/NavBar";
+import PrivateRoute from "./Components/PrivateRoute";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import ToDoList from "./Components/ToDoList";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <h1> Lambda List </h1>
+      <header>
+        <NavBar />
+      </header>
+      <section className="section">
+        <h1> Lambda List </h1>
+      </section>
+      <Router>
+        <Switch>
+          <PrivateRoute exact path="/todolist" component={ToDoList} />
+        </Switch>
+      </Router>
     </div>
   );
 }
