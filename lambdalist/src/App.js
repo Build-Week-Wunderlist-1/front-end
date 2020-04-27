4702a8f2518688d9cb8164c75997c6e06e0c27ac
nataliastewart <nataliastewart@users.noreply.github.com>
2020-04-27 15:36:48 -0400

    NavBar and privateroute created - styling into NavBar done

--- lambdalist/src/App.js
+++ lambdalist/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <div className="App">
-     <h1> Lambda List </h1>
+      <NavBar />
+      <h1> Lambda List </h1>
     </div>
   );
 }
