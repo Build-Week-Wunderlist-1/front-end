2d9c5fc93d123a5dc55c17e904cf61bf3d402c5a
Ernie Nettles <ernienettles@gmail.com>
2020-04-28 03:24:56 -0400

    Starting styling

--- lambdalist/src/App.js
+++ lambdalist/src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import SignUp from "./Components/SignUp";
+import Login from "./Components/Login";
+import particles from './particles.json';
+import Styled from 'styled-components';
 
 function App() {
+  particlesJS.load("particles-js", "assets/particles.json", function () {
+    console.log("callback - particles-js config loaded");
+  });
   return (
-    <div className="App">
-     <h1> Lambda List </h1>
-    </div>
+    
+      
+      <div className="App">
+      <div id="particles-js"></div>
+      <script src="particles.js"></script>
+      <script>
+      {particlesJS.load('particles-js', particles, () => {
+        console.log('particles.json loaded')
+      })}
+      </script>
+        <SignUp />
+        <Login />
+      </div>
+    
   );
 }
 
