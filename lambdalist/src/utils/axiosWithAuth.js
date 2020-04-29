2581ca6cc39b7b7a583589e73e3f068622c99b9c
nataliastewart <nataliastewart@users.noreply.github.com>
2020-04-29 18:15:35 -0400

    No erros - axios error solved - getting ready to deploy

--- lambdalist/src/utils/axiosWithAuth.js
+++ lambdalist/src/utils/axiosWithAuth.js
@@ -1,4 +1,4 @@
-import axios from "./node_modules/axios"
+import axios from "axios";
 export const axiosWithAuth = () => {
     const token = window.localStorage.getItem('token')
     return axios.create({
